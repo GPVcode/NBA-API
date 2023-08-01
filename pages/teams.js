b90bed699faa3e8cf2abdb021611c9cb62de376b
GPV <gvill005@gmail.com>
2023-07-31 21:32:23 -0700

    Get all teams function works

--- pages/teams.js
+++ pages/teams.js
@@ -1,8 +1,10 @@
 // TEAMS API
 // const getTeamsEl = document.getElementById('get-teams');
 const teamsTable = document.getElementById('table');
+
 //  onclick, create select options by looping through api data.
 const selectEl = document.querySelector('select');
+const formEl = document.getElementById('selectTeamForm');
 
 const selectTeams = async () => {
 
@@ -11,36 +13,76 @@ const selectTeams = async () => {
     const data = await response.json();
     const teamData = data.data;
 
+    let choice;
     for(let team of teamData){
         const teamName = team.full_name
-        if(selectEl.length === 31) return;
-        else selectEl.options[selectEl.options.length] = new Option(teamName);
+        if(selectEl.length === 31){
+            return;
+        } 
+        else{
+            selectEl.options[selectEl.options.length] = new Option(teamName);
+            // selectEl.options[selectEl.options.length].value = `${teamName}`;
+        } 
+        choice = teamName;
     }
 
-    console.log("what team?", teamsTable.options[teamsTable.selectedIndex].text)
 }
+
+formEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if(selectEl.value === "All Teams"){
+        getAllTeams();
+    } else {
+        getTeam();
+    }
+
+})
 
 const getAllTeams = async () => {
-    const table = document.getElementById('table');
-    const row = table.insertRow(table.length);
-    const abbreviation = row.insertCell(0);
-    const city = row.insertCell(1);
-    const conference = row.insertCell(2);
-    const division = row.insertCell(3);
-    const teamName = row.insertCell(4);
 
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-}
-  
-const requestTeams = async () => {
+    const tbl = document.getElementById('table');
+
     const url = `https://www.balldontlie.io/api/v1/teams`;
     const response = await fetch(url);
     const data = await response.json();
     const teamData = data.data;
-    return teamData[0]
+    
+    let team;
+
+    for(let i = 0; i < 30; i++){
+        const row = document.createElement("tr");
+        team = Object.entries(teamData[i]);
+
+        for(let j = 1; j < 6; j++){
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(`${team[j][1]}`);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        // add the row to the end of the table
+        tbl.appendChild(row);
+    }
+    
 }
 
-selectEl.addEventListener('onclick', selectTeams());
+// const getTeam = async () => {
+//     const url = `https://www.balldontlie.io/api/v1/teams`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     const teamData = data.data;
+//     return teamData[0]
+// }
+
+// selectEl.addEventListener('onclick', selectTeams());
+
+
+
+
+
 // getTeamsEl.addEventListener('onclick', getAllTeams());
 
+// get element ID of table
+// const teamsTable = document.getElementById('table');
+// add table row
+// add table row data
